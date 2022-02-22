96f73d014ce3d835b461d49ae48d88c09e27f162
S1lentcodes <Silentcodes@protonmail.com>
2022-02-21 20:07:35 -0500

    Changing qb-shops to using targeting

--- client/main.lua
+++ client/main.lua
@@ -20,21 +20,6 @@ local function SetupItems(shop)
     return items
 end
 
-local function DrawText3Ds(x, y, z, text)
-    SetTextScale(0.35, 0.35)
-    SetTextFont(4)
-    SetTextProportional(1)
-    SetTextColour(255, 255, 255, 215)
-    SetTextEntry("STRING")
-    SetTextCentre(true)
-    AddTextComponentString(text)
-    SetDrawOrigin(x,y,z, 0)
-    DrawText(0.0, 0.0)
-    local factor = (string.len(text)) / 370
-    DrawRect(0.0, 0.0+0.0125, 0.017+ factor, 0.03, 0, 0, 0, 75)
-    ClearDrawOrigin()
-end
-
 -- Events
 
 RegisterNetEvent('qb-shops:client:UpdateShop', function(shop, itemData, amount)
@@ -53,73 +38,12 @@ RegisterNetEvent('qb-shops:client:RestockShopItems', function(shop, amount)
     end
 end)
 
--- Threads
-
-CreateThread(function()
-    while true do
-        local InRange = false
-        local PlayerPed = PlayerPedId()
-        local PlayerPos = GetEntityCoords(PlayerPed)
-
-        for shop, _ in pairs(Config.Locations) do
-            local position = Config.Locations[shop]["coords"]
-            local products = Config.Locations[shop].products
-            for _, loc in pairs(position) do
-                local dist = #(PlayerPos - vector3(loc["x"], loc["y"], loc["z"]))
-                if dist < 10 then
-                    InRange = true
-                    DrawMarker(2, loc["x"], loc["y"], loc["z"], 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.2, 0.1, 255, 255, 255, 155, 0, 0, 0, 1, 0, 0, 0)
-                    if dist < 1 then
-                        DrawText3Ds(loc["x"], loc["y"], loc["z"] + 0.15, Lang:t("info.interact"))
-                        if IsControlJustPressed(0, 38) then -- E
-                            local ShopItems = {}
-                            ShopItems.items = {}
-                            QBCore.Functions.TriggerCallback("qb-shops:server:getLicenseStatus", function(hasLicense, hasLicenseItem)
-                                ShopItems.label = Config.Locations[shop]["label"]
-                                if Config.Locations[shop].products == Config.Products["weapons"] then
-                                    if hasLicense and hasLicenseItem then
-                                        ShopItems.items = SetupItems(shop)
-                                        QBCore.Functions.Notify(Lang:t("success.dealer_verify"), "success")
-                                        Wait(500)
-                                    else
-                                        for i = 1, #products do
-                                            if not products[i].requiredJob then
-                                                if not products[i].requiresLicense then
-                                                    ShopItems.items[#ShopItems.items+1] = products[i]
-                                                end
-                                            else
-                                                for i2 = 1, #products[i].requiredJob do
-                                                    if QBCore.Functions.GetPlayerData().job.name == products[i].requiredJob[i2] and not products[i].requiresLicense then
-                                                        ShopItems.items[#ShopItems.items+1] = products[i]
-                                                    end
-                                                end
-                                            end
-                                        end
-                                        QBCore.Functions.Notify(Lang:t("error.dealer_decline"), "error")
-                                        Wait(500)
-                                        QBCore.Functions.Notify(Lang:t("error.talk_cop"), "error")
-                                        Wait(1000)
-                                    end
-                                else
-                                    ShopItems.items = SetupItems(shop)
-                                end
-                                for k, v in pairs(ShopItems.items) do
-                                    ShopItems.items[k].slot = k
-                                end
-                                ShopItems.slots = 30
-                                TriggerServerEvent("inventory:server:OpenInventory", "shop", "Itemshop_"..shop, ShopItems)
-                            end)
-                        end
-                    end
-                end
-            end
-        end
-
-        if not InRange then
-            Wait(5000)
-        end
-        Wait(5)
-    end
+RegisterNetEvent('qb-shops:client:supermarkets', function()
+    local ShopItems = {}
+    ShopItems.items = {}
+    ShopItems.label = "24/7 supermarket"
+    ShopItems.items = SetupItems("247supermarket")
+    TriggerServerEvent("inventory:server:OpenInventory", "shop", "Itemshop_".."247supermarket", ShopItems)
 end)
 
 CreateThread(function()
